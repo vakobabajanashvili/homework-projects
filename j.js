43e59e2b515dde628965284c840a5fb0cc261995
vako <babajana.babajana112@gmail.com>
2024-02-20 23:20:31 +0400

    first commit

---
+++ j.js
@@ -0,0 +1,13 @@
+let age = 6
+
+if (age < 1) {
+    console.log("You Are Newborn");
+}
+
+else if(age == 6) {
+    console.log("Congrats Now You Can Go To The School And Work on Minimum Salary For Rest Of Your Life")
+}
+
+else {
+    console.log("You Are Not New Born");
+}
